b7ba48ab891b8edd6ac32b3598d22748943f96a2
Steven Loaiza <steven.loaiza@outlook.com>
2024-07-21 19:09:34 -0400

    Fix python file format

--- simpleReactAgent/simpleagent.py
+++ simpleReactAgent/simpleagent.py
@@ -5,22 +5,25 @@ from dotenv import load_dotenv
 _ = load_dotenv()
 from openai import OpenAI
 
+client = OpenAI()
+
 class Agent:
 	def __init__(self, system=""):
 		self.system = system
 		self.messages = []
 		if self.system:
 			self.messages.append({"role": "system", "content": system})
+
 	def __call__(self, message):
 		self.messages.append({"role": "user", "content": message})
 		result = self.execute()
-        	self.messages.append({"role": "assistant", "content": result})
-        	return result
+		self.messages.append({"role": "assistant", "content": result})
+		return result
 
-    	def execute(self):
-        	completion = client.chat.completions.create(
+	def execute(self):
+		completion = client.chat.completions.create(
                         model="gpt-4o", 
                         temperature=0,
                         messages=self.messages)
-        	return completion.choices[0].message.content
+		return completion.choices[0].message.content		
 
