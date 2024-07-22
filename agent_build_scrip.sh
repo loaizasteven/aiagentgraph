973756f643371a74690cf54430a46cb52e361f9d
Steven Loaiza <steven.loaiza@outlook.com>
2024-07-22 16:31:35 -0400

    Update bash script

--- agent_build_scrip.sh
+++ agent_build_scrip.sh
@@ -3,9 +3,13 @@
 # Repo AI-Agent-in-LangGraph
 # env
 DIRECTORY="agent_env"
+
+function activate () {
+  source $HOME/git/AI-Agents-in-LangGraph/agent_env/bin/activate
+}
+
 if [ -d "$DIRECTORY" ]; then
-    echo "agent venv exist, activating the env"
-    source "$DIRECTORY"/bin/activate
+    activate
 else
     echo "Creating venv"
     /usr/local/bin/python3 -m venv "$DIRECTORY"
