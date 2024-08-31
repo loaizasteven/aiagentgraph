550399e67a73c45da3080f7cdf8f64e71a5d8c67
Steven Loaiza <steven.loaiza@outlook.com>
2024-08-31 17:44:10 -0400

    Update venv agent build

--- libs/agent_build_scrip.sh
+++ libs/agent_build_scrip.sh
@@ -5,14 +5,14 @@
 DIRECTORY="agent_env"
 
 function activate () {
-  source $HOME/git/agentfrontend/$DIRECTORY/bin/activate
+  source $HOME/git/AI-Agents-in-LangGraph/$DIRECTORY/bin/activate
   # logging info
   echo "Currently activated $VIRTUAL_ENV"
   python --version
   # pip installation
   pip install --upgrade pip
   echo "pip installation in quiet mode..."
-  pip install -q -r requirements.txt --upgrade-strategy only-if-needed
+  pip install -q -r libs/requirements.txt --upgrade-strategy only-if-needed
 }
 
 if [ -d "$DIRECTORY" ]; then
